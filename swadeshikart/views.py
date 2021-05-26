9b1941b61c2b4013ccfeb5c611b4d85e52d73aff
Rishabhdhyani <imt_2018081@iiitm.ac.in>
2021-05-26 14:27:15 +0530

    Added Popular Products

--- swadeshikart/views.py
+++ swadeshikart/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from store.models import Product, ReviewRating
 
+def func(k):
+    return k[0]
+
 def home(request):
-    products = Product.objects.all().filter(is_available=True).order_by('created_date')[:8]
+    products = Product.objects.all().filter(is_available=True).order_by('created_date')
+    list_product = []
     for product in products:
-        reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
+        temp = []
+        temp.append(product.averageReview())
+        temp.append(product)
+        list_product.append(temp)
+
+    list_product.sort(reverse=True,key=func)
+    list_product = list_product[0:8]
+    #print(list_product)
+    ans_list=[]
+    print(list_product)
+    for i in list_product:
+        ans_list.append(i[1])
     context = {
        'products':products,
-       'reviews':reviews,
+       'ans_list':ans_list
     }
     return render(request,"home.html",context)
