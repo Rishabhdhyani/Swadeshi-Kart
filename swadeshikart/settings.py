77a0ba1c13fa1d6a61ad9a7fd48636293dfc9bd7
Rishabhdhyani <imt_2018081@iiitm.ac.in>
2021-05-28 07:55:16 +0530

    allowed host

--- swadeshikart/settings.py
+++ swadeshikart/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-env-Skart.eba-vipew8gv.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
