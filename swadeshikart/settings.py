bc71b811e1449a1276bfd49c37b9cd22534a77f4
Rishabhdhyani <imt_2018081@iiitm.ac.in>
2021-05-28 06:33:10 +0530

    delete

--- swadeshikart/settings.py
+++ swadeshikart/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-env.eba-7syjjm7m.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
