7fb0a98f1e307ff315540b5bda6df4f6cec78518
Rishabhdhyani <imt_2018081@iiitm.ac.in>
2021-05-29 11:36:52 +0530

    deploy

--- swadeshikart/settings.py
+++ swadeshikart/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import django_heroku
 
 from pathlib import Path
 from decouple import config
@@ -171,3 +172,5 @@ EMAIL_PORT = os.environ['EMAIL_PORT']
 EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 EMAIL_USE_TLS = os.environ['EMAIL_USE_TLS']
+
+django_heroku.settings(locals())
